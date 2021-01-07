95caeb0e7cb55d27e74a1cecc4afe66b1410da7f
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-01-07 18:09:26 +0300

    Added solution for task: createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,21 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const pathArray = path.split('.');
 
+  return obj => {
+    let result = obj;
+
+    for (let item of pathArray) {
+      const objProperty = result[item];
+
+      result = objProperty;
+
+      if (typeof objProperty === 'undefined') {
+        break;
+      }
+    }
+
+    return result;
+  };
 }
